541a6d61d968bbcfb9cb193beec9d56ac7cb92c1
Chris Busillo <info@shinycomputers.com>
2024-10-21 15:40:31 -0400

    Add QR Scanner module type definitions and integrate QR scanning in QRCodeWidget
    This commit introduces type definitions for the 'qr-scanner' module. Additionally, it integrates QR scanning functionality into the `QRCodeWidget`, replacing and cleaning up old scanning logic and formatting the code for consistency.

--- product_connect/static/src/js/widgets/qr_code_widget.js
+++ product_connect/static/src/js/widgets/qr_code_widget.js
@@ -1,10 +1,11 @@
 /** @odoo-module **/
-import {onMounted, onWillUnmount, useRef, useState} from '@odoo/owl';
-import {ConfirmationDialog} from '@web/core/confirmation_dialog/confirmation_dialog';
-import {CharField, charField} from '@web/views/fields/char/char_field';
-import {registry} from '@web/core/registry';
+import { onMounted, onWillUnmount, useRef, useState } from '@odoo/owl';
+import { ConfirmationDialog } from '@web/core/confirmation_dialog/confirmation_dialog';
+import { CharField, charField } from '@web/views/fields/char/char_field';
+import { registry } from '@web/core/registry';
 
-/** @typedef {import('qr-scanner').default} QrScannerType */
+/** @type {typeof import('qr-scanner').default} */
+const QrScanner = window.QrScanner;
 
 class QRCodeWidget extends CharField {
     static template = 'product_connect.QRCodeWidget';
@@ -23,14 +24,13 @@ class QRCodeWidget extends CharField {
 
         this.onScanSuccess = this.onScanSuccess.bind(this);
         onMounted(async () => {
-            const qrScanner = window.QrScanner
-            this.qrScanner = new qrScanner(
+            this.qrScanner = new QrScanner(
                 this.qrReaderRef.el,
                 this.onScanSuccess,
-                {returnDetailedScanResult: true}
+                { returnDetailedScanResult: true }
             )
 
-            await this.startScanner()
+            this.startScanner()
             if (this.qrScanner.hasFlash().catch((error) => this.logError(error))) {
                 this.state.flashlightLabel = "Flash on"
             }
@@ -47,11 +47,11 @@ class QRCodeWidget extends CharField {
         };
         try {
             this.stopScanner();
-            this.props.record.update({[this.props.name]: result.data})
+            this.props.record.update({ [this.props.name]: result.data })
                 .catch((error) => {
                     this.env.services.dialog.add(ConfirmationDialog, {
                         title: 'Error',
-                        body: error.data.message,
+                        body: error,
                         confirm: handleConfirm,
                     });
                 })
@@ -65,45 +65,21 @@ class QRCodeWidget extends CharField {
         this.state.buttonLabel = 'Stop'
         this.state.scanEnabled = true;
         this.qrReaderRef.el.classList.remove('d-none');
-
-        //     const currentState = this.htmlQrCode.getState()
-        //     if (currentState === Html5QrcodeScannerState.PAUSED) {
-        //         this.htmlQrCode.resume()
-        //     } else {
-        //         this.htmlQrCode.start(
-        //             { facingMode: "environment" },
-        //             {
-        //                 fps: 60,
-        //             },
-        //             this.onScanSuccess
-        //         )
-        //     }
     }
 
-    //
     stopScanner() {
         this.qrScanner.stop();
         this.state.buttonLabel = 'Scan'
         this.state.scanEnabled = false;
         this.qrReaderRef.el.classList.add('d-none');
-        //     this.htmlQrCode?.pause(true)
     }
 
     toggleScan() {
         if (this.state.scanEnabled) {
             this.stopScanner()
-            //     const currentState = this.htmlQrCode.getState();
-            //     // noinspection JSUnresolvedReference
-            //     if (currentState === Html5QrcodeScannerState.SCANNING) {
-            //         this.stopScanner()
-            //     }
         } else {
             this.startScanner()
-            //     this.startScanner().catch((error) => {
-            //         console.error('An error occurred:', error)
-            //     })
         }
-        // this.state.scanEnabled = !this.state.scanEnabled;
     }
 
     toggleFlashlight() {
