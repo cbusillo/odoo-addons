dfb065acc0217f942384b9d64d4a27b311340d35
Chris Busillo <info@shinycomputers.com>
2024-11-11 17:01:14 -0500

    Refactor to simplify search domain in _read_group_stages method
    Removed redundant search_domain variable to streamline the code. This change ensures cleaner and more efficient querying of motor stages.

--- product_connect/models/motor.py
+++ product_connect/models/motor.py
@@ -63,8 +63,7 @@ class Motor(models.Model):
 
     @api.model
     def _read_group_stages(self, *_args) -> "odoo.model.motor_stage":
-        search_domain = []
-        all_stages = self.env["motor.stage"].search(search_domain)
+        all_stages = self.env["motor.stage"].search([])
         self.env["motor"].search([("stage", "=", False), ("active", "in", [True, False])]).write(
             {"stage": all_stages.search([("name", "=", "Checkin")], limit=1).id}
         )
