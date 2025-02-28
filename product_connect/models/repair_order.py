0660f05ba48f678dd595fa96be9aa2062592401a
Chris Busillo <info@shinycomputers.com>
2025-02-28 15:17:10 -0500

    Add cost recalculation logic to `action_repair_done` method
    This update adjusts the standard price of a product by calculating costs from repair moves. It ensures accurate cost handling based on stock availability or initial quantity after a repair is marked as done.

--- product_connect/models/repair_order.py
+++ product_connect/models/repair_order.py
@@ -7,3 +7,18 @@ class RepairOrder(models.Model):
     motor = fields.Many2one(
         "motor", related="product_id.motor", store=True, index=True, readonly=True, ondelete="restrict"
     )
+
+    def action_repair_done(self):
+        res = super().action_repair_done()
+        for order in self:
+            for move in order.move_ids:
+                pass
+                # decrement quantity from shopify
+            cost = sum([m.product_tmpl_id.standard_price * m.quantity for m in order.move_ids])
+            cost_per_unit = (
+                cost / order.product_id.product_tmpl_id.qty_available
+                if order.product_id.product_tmpl_id.is_ready_for_sale
+                else order.product_id.product_tmpl_id.initial_quantity
+            )
+            order.product_id.standard_price += cost_per_unit
+        return res
