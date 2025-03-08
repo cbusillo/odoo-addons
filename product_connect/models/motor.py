69ab90e44f030a15cbbb63c60040045d609b2db3
Chris Busillo <info@shinycomputers.com>
2025-03-07 21:26:19 -0500

    Refactor `_compute_icon` to correctly handle image_1920 dependency.
    Updated the `_compute_icon` method to depend on `images.image_1920`, ensuring proper field tracking. Adjusted the logic to fetch the first available `image_1920`, improving data consistency and clarity.

--- product_connect/models/motor.py
+++ product_connect/models/motor.py
@@ -308,10 +308,10 @@ class Motor(models.Model):
         for motor in self:
             motor.has_notes = bool(motor.notes)
 
-    @api.depends("images")
+    @api.depends("images.image_1920")
     def _compute_icon(self) -> None:
         for motor in self:
-            motor.image_icon = motor.images[0].image_128 if motor.images else False
+            motor.image_icon = next((image.image_1920 for image in motor.images if image.image_1920), False)
 
     @api.depends("horsepower")
     def _compute_horsepower_formatted(self) -> None:
